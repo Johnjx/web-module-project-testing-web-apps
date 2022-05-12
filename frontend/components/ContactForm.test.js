3a54b01e02d4785e2ec037d0854f432ecbbd4b52
John Ruddock <john.jxr@pm.me>
2022-05-12 15:24:18 -0400

    1 error message when user enter less than 5 chars into first name input

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -19,7 +19,13 @@ test('renders the contact form header', () => {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    render(<ContactForm />);
+    const firstNameInput = screen.getByLabelText(/first name*/i);
+    const firstName = "J";
+    userEvent.type(firstNameInput, firstName);
 
+    const errorMessages = await screen.findAllByTestId("error");
+    expect(errorMessages).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
